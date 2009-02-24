cb4ac7e30e88b233ecc9854134b8a21960ea1ea1
Martin Vejnar <avakar@technika.junior.cz>
2009-02-24 15:55:46 +0100

    Added 'propsp' matcher and all negative matchers.

--- matchers.py
+++ matchers.py
@@ -5,10 +5,25 @@ def any_matcher(ch):
 def newline_matcher(ch):
     return ch in '\n\r'
 
-default_matchers = {
+def proper_space_matcher(ch):
+    return ch.isspace() and not newline_matcher(ch)
+
+_default_matchers = {
     'any': any_matcher,
     'space': str.isspace,
     'digit': str.isdigit,
     'alnum': str.isalnum,
     'newline': newline_matcher,
+    'propsp': proper_space_matcher,
 }
+
+class InverseMatcher:
+    def __init__(self, matcher):
+        self.matcher = matcher
+    
+    def __call__(self, ch):
+        return not self.matcher(ch)
+
+default_matchers = dict(_default_matchers)
+for name, matcher in _default_matchers.iteritems():
+    default_matchers['-' + name] = InverseMatcher(matcher)
