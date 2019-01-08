175417b78ee999eedd7ea786ee6cd057f68a2b0d
Martin Vejnár <vejnar.martin@gmail.com>
2019-01-08 19:02:32 +0100

    Add classifiers

--- setup.py
+++ setup.py
@@ -18,4 +18,17 @@ setup(name='limecc',
             ],
         },
     test_suite = "limecc.tests",
+    classifiers=[
+        # Supported python versions
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+
+        # License
+        'License :: OSI Approved :: Boost Software License 1.0 (BSL-1.0)',
+
+        # Topics
+        'Topic :: Software Development :: Libraries',
+    ]
     )
